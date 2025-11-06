031e77dfe3cf5d210f03bfcf29bef583d5912b91
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-11-06 20:15:14 +0600

    feat(server): setup Express server with dotenv, CORS, cookie-parser, and database connection

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Global Middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // e.g., http://localhost:5173
+    credentials: true,
+    methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+
+// Start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
