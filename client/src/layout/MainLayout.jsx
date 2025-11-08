19a41f7a491b3fad2b16f1309efdb9524f34a857
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-11-08 11:19:32 +0600

    feat(layout): Add MainLayout component

---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+
+
+const MainLayout = () => {
+  return (
+        <>
+      <Header />
+      <main className="pt-[73px] min-h-screen">
+        <Outlet />
+      </main>
+    
+      <Footer />
+    </>
+  )
+}
+
+export default MainLayout
